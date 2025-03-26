73a5b16772f9b91e70e20a9052dc777ff85b353b
Emma Kadlecek <kadlecekemma@gmail.com>
2025-03-26 14:45:08 -0700

    .

--- src/flog.rs
+++ src/flog.rs
@@ -1,8 +1,38 @@
+use std::{fs::File, path::Path};
 use crate::levels::LogLevel;
 
+
 pub fn jlog(path: String, msg: String, level: LogLevel) {
+    let tpath = Path::new(&path);
+
+    match tpath.exists() {
+        true => {
+            logoldfile(path,msg,level);
+        }
+
+        false => {
+            lognewfile(path, msg,level);
+        }
+    } 
+}
 
 
+fn logoldfile(path: String, msg: String, level: LogLevel) {
+    todo!()
+}
 
+fn lognewfile(path: String, msg: String, level: LogLevel) {
 
+    let tpath = Path::new(&path);
+    let display = tpath.display();
+     let mut file = match File::create(&tpath) {
+        Err(why) => panic!("couldn't create {}: {}", display, why),
+        Ok(file) => file,
+    };
+
+    // Write the `LOREM_IPSUM` string to `file`, returns `io::Result<()>`
+    match file.write_all(LOREM_IPSUM.as_bytes()) {
+        Err(why) => panic!("couldn't write to {}: {}", display, why),
+        Ok(_) => println!("successfully wrote to {}", display),
+    }
 }
