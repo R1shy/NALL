bec9c364cf02214801af9dae4096a8c9c533c31f
Emma Kadlecek <kadlecekemma@gmail.com>
2025-03-30 15:39:30 -0700

    .

--- src/clog.rs
+++ src/clog.rs
@@ -2,10 +2,10 @@ use crate::levels;
 use std::process::exit;
 
 pub fn clog(level: levels::LogLevel, msg: String) {
-    let warnc = "\x1b[222mWARN\x1b[0m";
-    let infoc = "\x1b[63mINFO\x1b[0m";
-    let errc = "\x1b[1mERR\x1b[0m";
-    let fatalc = "\x1b[52mFATAL\x1b[0m";
+let warnc = "\x1b[38;5;222mWARN\x1b[0m"; 
+let infoc = "\x1b[38;5;63mINFO\x1b[0m";
+let errc = "\x1b[38;5;1mERR\x1b[0m"; 
+let fatalc = "\x1b[38;5;52mFATAL\x1b[0m";
 
     match level {
         levels::LogLevel::Warn => {
