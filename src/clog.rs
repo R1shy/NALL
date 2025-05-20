4f74ad7f330a4528958f08c0f2505a74f41c1813
Emma Kadlecek <kadlecekemma@gmail.com>
2025-05-20 15:13:56 -0700

    .

--- src/clog.rs
+++ src/clog.rs
@@ -2,7 +2,14 @@ use crate::levels;
 use std::process::exit;
 
 pub fn clog(level: levels::LogLevel, msg: String, color: Option<i32>) {
-    let col: i32 = color.unwrap_or(20);
+    let default_color: i32;
+    match level {
+        crate::LogLevel::Warn => {default_color = 221;}
+        crate::LogLevel::Err => {default_color = 196;}
+        crate::LogLevel::Info => {default_color = 33;}
+        crate::LogLevel::Fatal => {default_color = 88;}
+    } 
+    let col: i32 = color.unwrap_or(default_color);
     let warnc = format!("\x1b[38;5;{}mWARN\x1b[0m", col);
     let infoc = format!("\x1b[38;5;{}mINFO\x1b[0m", col);
     let errc = format!("\x1b[38;5;{}mERR\x1b[0m", col);
